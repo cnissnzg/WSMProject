e782966645357fa024a790a661b5aee410691cec
Ruize Gao <ruizgaonlp@gmail.com>
2022-06-11 16:40:33 +0800

    Add files via upload

--- searching/views.py
+++ searching/views.py
@@ -13,25 +13,29 @@ def index(request):
 
 def search(request):
     key = request.GET.get('key')
-    cate = request.GET.get('group')
+    group = request.GET.get('group')
+    cate = request.GET.get('category')
     error_msg = ''
     if not key:
         error_msg = "请输入关键词"
         return render(request, 'index.html', {'error_msg': error_msg})
 
-    if cate == '1':
+    if group == '1':
         bq = tool.rankedQuery(key, 15)
-    elif cate == '3':
+    elif group == '3':
         print(tool.termDict)
         bq = tool.booleanQuery(key.split())
-
     else:
         bq = tool.specificQuery(dateutil.parser.parse(key))
 
     print(bq)
     post_list = []
     for i in bq:
-        post_list.append(tool.docs[i])
+        if cate=='0':
+            post_list.append(tool.docs[i])
+        else:
+            if (tool.docs[i]['label'] + 1) == int(cate):
+                post_list.append(tool.docs[i])
 
     # post_list = Post.objects.filter(Q(title__icontains=key) | Q(content__icontains=key))
     return render(request, 'index.html', {'error_msg': error_msg, 'post_list': post_list})
